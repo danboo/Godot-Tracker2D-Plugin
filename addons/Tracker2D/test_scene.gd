a6271c48426dbaa58252dbacaa8463bd2c2eadd4
danboo <dan@boorstein.net>
2022-12-18 21:55:40 -0900

    tidy up test scene

--- addons/Tracker2D/test_scene.gd
+++ addons/Tracker2D/test_scene.gd
@@ -4,6 +4,18 @@ var tracker_scene : PackedScene = preload("tracker_2d.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	test_node2d()
+	test_sprite()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.global_rotation += 1.0 * delta
+
+func test_sprite() -> void:
+	var sprite_tracker : Tracker2D = tracker_scene.instantiate()
+	$sprite.add_child( sprite_tracker )
+
+func test_node2d() -> void:
 	var node1 = Node2D.new();
 	node1.name = "Rotator 1"
 	add_child( node1 );
@@ -11,31 +23,3 @@ func _ready():
 
 	var tracker : Tracker2D = tracker_scene.instantiate()
 	node1.add_child( tracker )
-	
-	var node2 = Node2D.new();
-	node2.name = "Rotator 2"
-	add_child( node2 );
-	node2.position = Vector2(-100,-100)
-
-	var node3 = Node2D.new();
-	node3.name = "Invisible Name"
-	add_child( node3 );
-	node3.position = Vector2(100,-100)
-	
-	var node4 = Marker2D.new();
-	node4.name = "Invisible Class"
-	add_child( node4 );
-	node4.position = Vector2(-100,100)
-
-	var node5 = Node2D.new();
-	node5.name = "Should Be Invisible"
-	add_child( node5 );
-	node5.position = Vector2(100,-100)
-
-	var sprite_tracker : Tracker2D = tracker_scene.instantiate()
-	$sprite.add_child( sprite_tracker )
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	self.global_rotation += 0.5 * delta
